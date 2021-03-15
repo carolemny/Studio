a9fd9987f684975cbf9e227bd34dd5bf7ee94b28
Carole Meney <meneycarole@gmail.com>
2021-03-15 17:56:17 +0100

    Add users_controller and update permit routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   resources :bookings
-  root "spaces#index"
+  root to: "spaces#index"
   resources :spaces
+  resources :users, only: [:show, :edit, :update]
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
