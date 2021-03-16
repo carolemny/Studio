c8e242b6e6a4ce31eff3fa03ea51ba11d24cbc7c
perrine1611 <p.toubeau@gmail.com>
2021-03-16 12:46:46 +0100

    modified welcome email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -5,8 +5,9 @@ class UserMailer < ApplicationMailer
 
       @user = user 
   
-      @url  = 'https://studiofinalproject.herokuapp.com/' 
+      @url = 'https://studiofinalproject.herokuapp.com/users/sign_in'
   
       mail(to: @user.email, subject: 'Bienvenue chez STUDIO') 
     end
+
 end
