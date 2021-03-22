18152d533fd4a11d2ca8f4684ff1f75a91e24b4e
carolemny <meneycarole@gmail.com>
2021-03-22 20:41:56 +0100

    Adding end_date to Bookings and Update def to availables dates

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_19_101642) do
+ActiveRecord::Schema.define(version: 2021_03_22_140629) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,7 +42,7 @@ ActiveRecord::Schema.define(version: 2021_03_19_101642) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.datetime "start_date"
-    t.integer "duration"
+    t.date "end_date"
     t.index ["guest_id"], name: "index_bookings_on_guest_id"
     t.index ["space_id"], name: "index_bookings_on_space_id"
   end
