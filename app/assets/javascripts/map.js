601cd4ce4f5baf4f53eb5309001f9a01be358370
m-tessier <morgane.tessier22@gmail.com>
2021-03-25 15:56:25 +0100

    update search bar and market in map

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,83 +1,73 @@
 const getMap = async (spaces) => {
 
-  let fullAddress = `${spaces[0].address}, ${spaces[0].city}, ${spaces[0].zip_code}, France` 
   let address = document.getElementById('places-search');
-  let address2 = address.value.replace(/[ (0-9)]/g, '');
-
-  if (spaces.length && !address2) {
-    address2 = spaces[0].fullAddress
+  const results = await provider.search({ query: address.value || 'Paris' });
+  if (!results[0]) {
+    document.getElementById('info-map').innerHTML = `<p class="my-text-primary"><small>La ville est mal renseignée. Merci d'utiliser l'autocomplétion.</small><p>`;
+  } else {
+    document.getElementById('info-map').innerHTML = ``;
   }
 
-  
-  const results = await provider.search({ query: fullAddress || 'Paris' });
-  
-  document.getElementById('map-container').innerHTML = "<div id='map'></div>"
-  
-  let map = L.map('map').setView([results[0].y, results[0].x], 16);
+  document.getElementById('map-container').innerHTML = "<div id='map'></div>";
+
+  let map = L.map('map').setView([results[0].y, results[0].x], 13);
   L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', { 
     attribution: '© OpenStreetMap contributors',
     maxZoom: 19
   }).addTo(map);
-
   
-  var demo_with_map = new autoComplete({
+  
+  new autoComplete({
     selector: '#places-search',
     minChars: 2,
     source: function(term, response) {
       fetch('https://geo.api.gouv.fr/communes?boost=population&fields=centre&nom=' + term)
-        .then(function(response) {
-          return response.text();
-        }).then(function(body) {
-          var json = JSON.parse(body);
-          var new_json = json.map(function(el) {
-            return {
-              label: el.nom,
-              value: el.code,
-              lat: el.centre.coordinates[1],
-              lon: el.centre.coordinates[0],
-              boundingbox: null
-            }
-          })
-          response(new_json);
-        });
+      .then(function(response) {
+        return response.text();
+      }).then(function(body) {
+        let json = JSON.parse(body);
+        let new_json = json.map(function(el) {
+          return {
+            label: el.nom,
+            value: el.code,
+            boundingbox: null
+          }
+        })
+        response(new_json);
+      });
     },
     renderItem: function(item, search) {
       search = search.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&');
-      var re = new RegExp("(" + search.split(' ').join('|') + ")", "gi");
-      var optional_bbox_attribute = '';
+      let re = new RegExp("(" + search.split(' ').join('|') + ")", "gi");
+      let optional_bbox_attribute = '';
       if (item.boundingbox) {
-        var bbox = [item.boundingbox[2], item.boundingbox[0], item.boundingbox[3], item.boundingbox[1]];
-        var optional_bbox_attribute = 'data-bbox="' + bbox.join(',') + '" ';
+        let bbox = [item.boundingbox[2], item.boundingbox[0], item.boundingbox[3], item.boundingbox[1]];
+        let optional_bbox_attribute = 'data-bbox="' + bbox.join(',') + '" ';
       }
       return '<div class="autocomplete-suggestion" ' + optional_bbox_attribute +
-        'data-lon="' + item.lon + '" data-lat="' + item.lat +
-        '" data-val="' + item.label + '">' +
-        item.label.replace(re, "<b>$1</b>") +
-        '</div>';
+      'data-lon="' + item.lon + '" data-lat="' + item.lat +
+      '" data-val="' + item.label + '">' +
+      item.label.replace(re, "<b>$1</b>") +
+      '</div>';
     },
-    onSelect: function(e, term, item) {
-      if (item.getAttribute('data-bbox') && (item.getAttribute('data-bbox').split(',')).length > 0) {
-        var extent = item.getAttribute('data-bbox').split(',');
-        if (extent.length > 0) {
-          extent = extent.map(function(el) {
-            return Number(el);
-          });
-        }
-        var bounds = [[extent[1], extent[0]], [extent[3], extent[2]]];
-
-        // zoom the map to the bounds
-        map.fitBounds(bounds);
-      } else {
-        var lat = Number(item.getAttribute('data-lat'));
-        var lon = Number(item.getAttribute('data-lon'));
-        map.setView(L.latLng(lat, lon), 12);
-      }
-    }
   });
 
 
+
+  if (!spaces[0]) {
+    document.getElementById('info-map').innerHTML = `<p class="my-text-primary"><small>Il n'y a pas encore d'espaces dans cette ville.</small><p>`;
+  } else {
+    document.getElementById('info-map').innerHTML = ``;
+  }
+
   spaces.forEach((space) => {
-    let marker = L.marker([space.latitude, space.longitude]).addTo(map);
+    let marker = L.marker([space.latitude, space.longitude], {
+      icon: L.divIcon({
+        html: '<i class="fas fa-map-marker-alt" style="color: #FF2D00; font-size: 30px;"></i>',
+        iconSize: [20, 70],
+        className: 'myDivIcon'
+      }) 
+    }).addTo(map);
     marker.bindPopup(`<b>${space.title}</b><br>${space.price} € par jour`);
   });
 
